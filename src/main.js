91d1ea357e9a747bb548c51c449e6f7052420699
しぇぱっち <shepabashi@gmail.com>
2018-04-30 21:10:49 +0900

    add auth store

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 
+import * as firebase from 'firebase'
+import config from './config/firebase.config'
+
+firebase.initializeApp(config)
+firebase.auth().onAuthStateChanged(user => {
+  store.commit('auth', user)
+})
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/store.js
+++ src/store.js
@@ -5,10 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    auth: {
+      login: false,
+      user: null
+    }
   },
   mutations: {
-
+    auth (state, user) {
+      state.auth.login = !!user
+      state.auth.user = {
+        uid: user.uid,
+        displayName: user.displayName,
+        email: user.email,
+        phoneNumber: user.phoneNumber,
+        photoURL: user.photoURL,
+        providerId: user.providerId
+      }
+    }
   },
   actions: {
 
