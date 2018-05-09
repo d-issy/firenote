b5d2d533082628c0001d1633d22f24f5d0e49a67
しぇぱっち <shepabashi@gmail.com>
2018-05-10 00:12:31 +0900

    add search bar

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search-box">
+    <form action="">
+      <input v-model="query" type="text" placeholder="Search note...">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      query: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.search-box input {
+  display: block;
+  max-width: 100%;
+  width: 450px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin: 15px auto;
+  font-size: 16px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="home">
       <h1>Note List</h1>
+      <SearchBox />
       <NoteList />
   </div>
 </template>
 
 <script>
-import NoteList from '../components/NoteList'
+import NoteList from '@/components/NoteList'
+import SearchBox from '@/components/SearchBox'
+
 export default {
   name: 'home',
   components: {
-    NoteList
+    NoteList,
+    SearchBox
   }
 }
 </script>
